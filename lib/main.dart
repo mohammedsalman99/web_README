1016d8c448d2c01809134178a9de91185df37c67
mohammed salman <159923000+mohammedsalman99@users.noreply.github.com>
2024-12-23 22:15:03 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:web_frontend/screens/login_page.dart';
 import 'package:web_frontend/screens/splash_screen.dart';
-// Replace with your actual login screen file
 
 void main() {
   runApp(MyApp());
